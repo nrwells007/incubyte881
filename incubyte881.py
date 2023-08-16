3b82237a5e51f2074281e88b1134a7d50defd673
Nilabhro Roy <69209697+nrwells007@users.noreply.github.com>
2023-08-16 12:44:57 +0530

    Incubyte Chandrayaan 3 TDD Assessment v1.0
    -> incubyte881.py contains the code for the navigation.
    -> test_spacecraft881.py contains the code for unit testing.

---
+++ incubyte881.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+class Spacecraft:
+    def __init__(self, initial_position, initial_direction):
+        self.position = initial_position
+        if(initial_direction in ["Up","Down"]):
+            self.vdirection = initial_direction
+            self.hdirection="N"
+        else:
+            self.hdirection = initial_direction
+            self.vdirection="Up"
+        
+        self.main_direction=initial_direction
+
+    def move_forward(self):
+        if self.main_direction == "N":
+            self.position[1] += 1
+        elif self.main_direction == "S":
+            self.position[1] -= 1
+        elif self.main_direction == "E":
+            self.position[0] += 1
+        elif self.main_direction == "W":
+            self.position[0] -= 1
+        elif self.main_directionn == "Up":
+            self.position[2] += 1
+        elif self.main_direction == "Down":
+            self.position[2] -= 1
+
+    def move_backward(self):
+        if self.main_direction == "N":
+            self.position[1] -= 1
+        elif self.main_direction == "S":
+            self.position[1] += 1
+        elif self.main_direction == "E":
+            self.position[0] -= 1
+        elif self.main_direction == "W":
+            self.position[0] += 1
+        elif self.main_direction == "Up":
+            self.position[2] -= 1
+        elif self.main_direction == "Down":
+            self.position[2] += 1
+
+    def turn_left(self):
+        if self.hdirection == "N":
+            self.hdirection = "W"
+            self.main_direction="W"
+        elif self.hdirection == "S":
+            self.hdirection = "E"
+            self.main_direction="E"
+        elif self.hdirection == "E":
+            self.hdirection = "N"
+            self.main_direction="N"
+        elif self.hdirection == "W":
+            self.hdirection = "S"
+            self.main_direction="S"
+
+    def turn_right(self):
+        if self.hdirection == "N":
+            self.hdirection = "E"
+            self.main_direction="E"
+        elif self.hdirection == "S":
+            self.hdirection = "W"
+            self.main_direction="W"
+        elif self.hdirection == "E":
+            self.hdirection = "S"
+            self.main_direction="S"
+        elif self.hdirection == "W":
+            self.hdirection = "N"
+            self.main_direction="N"
+
+    def rotate_up(self):
+        self.vdirection = "Up"
+        self.main_direction = "Up"
+
+    def rotate_down(self):
+        self.vdirection = "Down"
+        self.main_direction = "Down"
+
+
+# In[16]:
+
+
+def execute_commands(commands, initial_position, initial_direction):
+    spacecraft = Spacecraft(initial_position, initial_direction)
+    
+    for command in commands:
+        if command == "f":
+            spacecraft.move_forward()
+        elif command == "b":
+            spacecraft.move_backward()
+        elif command == "l":
+            spacecraft.turn_left()
+        elif command == "r":
+            spacecraft.turn_right()
+        elif command == "u":
+            spacecraft.rotate_up()
+        elif command == "d":
+            spacecraft.rotate_down()
+    
+    return spacecraft.position, spacecraft.main_direction
+
+
+# In[17]:
+
+
+initial_position = [0, 0, 0]
+initial_direction = "N"
+commands = ["f", "r", "u", "b", "l"]
+final_position, final_direction = execute_commands(commands, initial_position, initial_direction)
+print("Final Position:", final_position)
+print("Final Direction:", final_direction)
+
---
+++ test_spacecraft881.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[60]:
+
+
+import unittest
+from incubyte881 import Spacecraft, execute_commands
+
+class TestSpacecraft(unittest.TestCase):
+
+    def test_movement(self):
+        spacecraft = Spacecraft([0, 0, 0], "N")
+        
+        spacecraft.move_forward()
+        self.assertEqual(spacecraft.position, [0, 1, 0])
+        
+        spacecraft.move_backward()
+        self.assertEqual(spacecraft.position, [0, 0, 0])
+
+    def test_rotation(self):
+        spacecraft = Spacecraft([0, 0, 0], "N")
+        
+        spacecraft.turn_left()
+        self.assertEqual(spacecraft.main_direction, "W")
+        
+        spacecraft.turn_right()
+        self.assertEqual(spacecraft.main_direction, "N")
+
+    def test_rotation_vertical(self):
+        spacecraft = Spacecraft([0, 0, 0], "N")
+        
+        spacecraft.rotate_up()
+        self.assertEqual(spacecraft.main_direction, "Up")
+        
+        spacecraft.rotate_down()
+        self.assertEqual(spacecraft.main_direction, "Down")
+
+    def test_execute_commands(self):
+        initial_position = [0, 0, 0]
+        initial_direction = "N"
+        commands = ["f", "r", "u", "b", "l"]
+        final_position, final_direction = execute_commands(commands, initial_position, initial_direction)
+        self.assertEqual(final_position, [0, 1, -1])
+        self.assertEqual(final_direction, "N")
+
+if __name__ == '__main__':
+    unittest.main()
+
